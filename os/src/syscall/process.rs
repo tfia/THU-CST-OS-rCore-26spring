56d227e1783c8773c6377949034d768cc7ae7c26
tfia <duruohe@outlook.com>
2026-03-04 14:26:45 +0800

    feat: stride scheduling

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -185,11 +185,17 @@ pub fn sys_spawn(path: *const u8) -> isize {
     }
 }
 
-// YOUR JOB: Set task priority.
-pub fn sys_set_priority(_prio: isize) -> isize {
+// Set task priority.
+pub fn sys_set_priority(prio: isize) -> isize {
     trace!(
-        "kernel:pid[{}] sys_set_priority NOT IMPLEMENTED",
+        "kernel:pid[{}] sys_set_priority",
         current_task().unwrap().pid.0
     );
+    if prio >= 2 {
+        if let Some(task) = current_task() {
+            task.inner_exclusive_access().priority = prio as usize;
+            return prio;
+        }
+    }
     -1
 }
--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -71,6 +71,12 @@ pub struct TaskControlBlockInner {
 
     /// Program break
     pub program_brk: usize,
+
+    /// Program priority
+    pub priority: usize,
+
+    /// Program pass
+    pub pass: usize,
 }
 
 impl TaskControlBlockInner {
@@ -135,6 +141,8 @@ impl TaskControlBlock {
                     ],
                     heap_bottom: user_sp,
                     program_brk: user_sp,
+                    priority: 16,
+                    pass: 0,
                 })
             },
         };
@@ -216,6 +224,8 @@ impl TaskControlBlock {
                     fd_table: new_fd_table,
                     heap_bottom: parent_inner.heap_bottom,
                     program_brk: parent_inner.program_brk,
+                    priority: parent_inner.priority,
+                    pass: parent_inner.pass,
                 })
             },
         });
@@ -258,6 +268,8 @@ impl TaskControlBlock {
                     exit_code: 0,
                     heap_bottom: user_sp,
                     program_brk: user_sp,
+                    priority: 16,
+                    pass: 0,
                 })
             },
         });
