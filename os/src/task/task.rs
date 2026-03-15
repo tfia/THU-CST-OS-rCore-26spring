75e4e027b06911f8e5891687ac504814b584bb84
tfia <duruohe@outlook.com>
2026-03-15 10:53:16 +0800

    fix: child process kernel stack sp

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -254,7 +254,7 @@ impl TaskControlBlock {
             entry_point,
             user_sp,
             KERNEL_SPACE.exclusive_access().token(),
-            self.kernel_stack.get_top(),
+            kernel_stack_top,
             trap_handler as usize,
         );
 
